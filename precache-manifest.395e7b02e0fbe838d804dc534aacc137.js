bbf905dcdeb708de5107faba6798df2d6b4205bd
abhishekgiram <abhishek.giram11@gmail.com>
2020-03-13 01:16:23 +0530

    Updates

---
+++ precache-manifest.395e7b02e0fbe838d804dc534aacc137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d621d8fec698330d0217425a2df860",
+    "url": "/mychatapp/index.html"
+  },
+  {
+    "revision": "3bf718a41bd148b31df7",
+    "url": "/mychatapp/static/css/main.800f09eb.chunk.css"
+  },
+  {
+    "revision": "ef1ee01f758e87b67db5",
+    "url": "/mychatapp/static/js/2.01697789.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mychatapp/static/js/2.01697789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf718a41bd148b31df7",
+    "url": "/mychatapp/static/js/main.69d0f4d2.chunk.js"
+  },
+  {
+    "revision": "605b215f70bb92157a85",
+    "url": "/mychatapp/static/js/runtime-main.3a20dd4a.js"
+  }
+]);
